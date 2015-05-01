ee45967d8647556a8ca1ea1617898874b2158b51
juno <juno@krasava.fi>
2015-05-02 02:31:48 +0300

    dev

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: '4FiFipornoDesk'
+      module: 'FiFiDeskMobile'
     }))
     .pipe(gulp.dest(paths.tmp + '/partials/'));
 });
